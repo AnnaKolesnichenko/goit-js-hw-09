1de85c1e2d02e4fd6d1047289bee7ab323e25354
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-05-04 01:51:08 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,6 +7,7 @@ const hoursCount = document.querySelector('[data-hours]');
 const minutesCount = document.querySelector('[data-minutes]');
 const secondsCount = document.querySelector('[data-seconds]');
 
+
 let selectedDate = null;
 
 const countDown = flatpickr(timePicker, {
@@ -23,58 +24,42 @@ const countDown = flatpickr(timePicker, {
 function startTimer() {
     const now = new Date().getTime();
     const difference = selectedDate.getTime() - now;
-    console.log(difference);
+    
+    if(difference < 0) {
+        alert("Please choose a date in the future");
+        startBtn.disabled = true;
+    } 
+        startBtn.disabled = false;
+
+    const timerId = setInterval(() => {
+        const now = new Date().getTime();
+        const difference = selectedDate.getTime() - now;
+
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+    
+        const days = Math.floor(difference / day);
+        const hours = Math.floor((difference % day) / hour);
+        const minutes = Math.floor(((difference % day) % hour) / minute);
+        const seconds = Math.floor((((difference % day) % hour) % minute) / second);
+
+        daysCount.textContent = days;
+        hoursCount.textContent = hours;
+        minutesCount.textContent = minutes;
+        secondsCount.textContent = seconds;
+        //console.log(`${days}:${hours}:${minutes}:${seconds}`);
+    }, 1000);
+
+    if(difference == 0) {
+        clearInterval(timerId);
+    }
+
+    
 }
 
 startBtn.addEventListener('click', startTimer);
 
-/*const dateNow = new Date();
-const timeDifference = selectedDate.getTime() - dateNow.getTime();
-if(timeDifference < 0) {
-alert("Please choose a date in the future");
-startBtn.disabled = true;
-} startBtn.disabled = false;
 
 
-
-function onTimerUpdate() {
-    const setDate = "2023-05-10T00:00:00";
-
-    const timerId = setInterval(() => {
-        const endDate = options.defaultDate.getTime();
-        const difference = setDate - endDate;
-        
-        convertMs(difference);        
-    }, 1000)
-}
-
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    daysCount.innerHTML = days;
-    hoursCount.innerHTML = hours;
-    minutesCount.innerHTML = minutes;
-    secondsCount.innerHTML = seconds;
-  
-    //return { days, hours, minutes, seconds };
-  }
-      
-
-startBtn.addEventListener('click', onTimerUpdate);
-
-
-
-/*if(options.defaultDate < selectedDates[0]) {
-    window.alert("Please choose a date in the future");
-    startBtn.disabled = true;
-    return;
-} startBtn.disabled = false;*/
